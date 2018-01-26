bfb4e8579196b887e41ce0b5db8bf35c80f33043
Sam Francis <samfrancis.dev@gmail.com>
2018-01-26 16:26:06 -0500

    Added the Font protocol and extension

---
+++ Swash/Classes/Font.swift
@@ -0,0 +1,59 @@
+//
+//  Font.swift
+//  Swash
+//
+//  Created by Sam Francis on 1/26/18.
+//
+
+import UIKit
+
+@available(iOS 8.2, *)
+public protocol Font {
+    func of(size: CGFloat) -> UIFont?
+    
+    @available(iOS 11.0, *)
+    func of(textStyle: UIFontTextStyle, defaultSize: CGFloat) -> UIFont?
+    
+    @available(iOS 11.0, *)
+    func of(textStyle: UIFontTextStyle, defaultSize: CGFloat, maxSize: CGFloat) -> UIFont?
+    
+    @available(iOS, introduced: 8.2, deprecated: 11.0, message: "In iOS 11+, you must specify a default size for custom FontTypes")
+    func of(textStyle: UIFontTextStyle) -> UIFont?
+    
+    @available(iOS, introduced: 8.2, deprecated: 11.0, message: "In iOS 11+, you must specify a default size for custom FontTypes")
+    func of(textStyle: UIFontTextStyle, maxSize: CGFloat) -> UIFont?
+}
+
+@available(iOS 8.2, *)
+public extension Font where Self: RawRepresentable, Self.RawValue == String {
+    public func of(size: CGFloat) -> UIFont? {
+        return UIFont(name: rawValue, size: size)
+    }
+    
+    @available(iOS 11.0, *)
+    public func of(textStyle: UIFontTextStyle, defaultSize: CGFloat) -> UIFont? {
+        guard let font = of(size: defaultSize) else { return nil }
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+    }
+    
+    @available(iOS 11.0, *)
+    public func of(textStyle: UIFontTextStyle, defaultSize: CGFloat, maxSize: CGFloat) -> UIFont? {
+        guard let font = of(size: defaultSize) else { return nil }
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font, maximumPointSize: maxSize)
+    }
+    
+    @available(iOS, introduced: 8.2, deprecated: 11.0, message: "In iOS 11+, you must specify a default size for custom FontTypes")
+    public func of(textStyle: UIFontTextStyle) -> UIFont? {
+        return of(size: UIFont.preferredFont(forTextStyle: textStyle).pointSize)
+    }
+    
+    @available(iOS, introduced: 8.2, deprecated: 11.0, message: "In iOS 11+, you must specify a default size for custom FontTypes")
+    public func of(textStyle: UIFontTextStyle, maxSize: CGFloat) -> UIFont? {
+        guard let font = of(textStyle: textStyle) else { return nil }
+        if font.pointSize <= maxSize {
+            return font
+        } else {
+            return of(size: maxSize)
+        }
+    }
+}
